40a6f152eedbc1a54166904e5aa72fe46e3f8dd7
ahmed thousif <ahmedthousif571@gmail.com>
2025-11-18 21:50:41 +0530

    config for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
     proxy: {
